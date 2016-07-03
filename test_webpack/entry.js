de7a0880aff494f92940144a0c9c3a21626143af
tigerxjtu <liyin@16010.net>
2016-07-03 22:36:52 +0800

    webpack bootstrap react

---
+++ test_webpack/entry.js
@@ -0,0 +1,9 @@
+//window.jQuery=require('jquery');
+require('expose?$!expose?jQuery!jquery');
+require('bootstrap-webpack');
+
+require("./style.css");
+//document.write(require("./content.js"));
+document.write(`
+<h1>aaa</h1>
+	`);
