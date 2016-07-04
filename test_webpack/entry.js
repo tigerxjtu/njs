0795b9a85e8019235b630ca95c3fab55c6fae360
tigerxjtu <liyin@16010.net>
2016-07-04 21:01:56 +0800

    update webpack bootstrap react

--- test_webpack/entry.js
+++ test_webpack/entry.js
@@ -3,7 +3,47 @@ require('expose?$!expose?jQuery!jquery');
 require('bootstrap-webpack');
 
 require("./style.css");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 //document.write(require("./content.js"));
+
+
+class MyComponent extends React.Component{
+	render(){
+		const list=[1,2,3,4,5,6,7].map(n=>{
+			return (
+				<li className="list-group-item" key={n}>item {n} <span className="badge">{n}</span></li>
+
+			);
+		});
+		return (
+			<ul className="list-group">
+				{list}
+			</ul>
+		);
+	}
+}
+
+ReactDOM.render(<MyComponent />,document.body);
 document.write(`
 <h1>aaa</h1>
+<div>
+
+<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    Dropdown
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
+<button type="button" class="btn btn-primary" aria-label="Left Align">
+  <span class="glyphicon glyphicon-star" aria-hidden="true"></span>按钮
+</button>
+</div>
 	`);
--- test_webpack/webpack.config.js
+++ test_webpack/webpack.config.js
@@ -5,14 +5,32 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-    loaders: [
-      { test: /\.css$/, loader: "style!css" },
-      // the url-loader uses DataUrls. 
-      // the file-loader emits files. 
-      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
-  }
+        loaders: [{
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            // the url-loader uses DataUrls. 
+            // the file-loader emits files. 
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            }, {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            }, {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            }, {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            }, {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference 
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
 };
